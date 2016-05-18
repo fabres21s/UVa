fcb12ef14f003560090cd68430f0d6e4cabf1718
fabio.sierra <fabio.sierra@essecorp.net>
2016-05-18 13:57:05 -0500

    Pruebas de envío

--- i/src/Main100.java
+++ i/src/Main100.java
@@ -1,59 +1,56 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-/*
- *   The 3n+1 Problem
- *   UVa:  100
- *   * -Proceso matem�tico
- * Si n es par, dividirlo entre dos, si no multiplicarlo por 3 y sumarle uno
-   Hallar el n�mero al cual hay que hacerle m�s veces ese proceso en un rango dado   
+
+/**
+ * 
+ * @author fabio
+ * The 3n+1 Problem
+ * 
+ *  - Proceso matemático
+ * 	Si n es par, dividirlo entre dos, si no multiplicarlo por 3 y sumarle uno
+ *	Hallar el número al cual hay que hacerle más veces ese proceso en un rango dado
+ *	
+ * Fecha de envío : 2011-04-02 08:16
+ * Runtime   		: 1.036s
+ * Puesto:		: 46212
+ *
  */
-public class Main100 
-{
+public class Main100 {
 
 	/**
 	 * @param args
-	 * @throws IOException 
-	 * @throws NumberFormatException 
+	 * @throws IOException
+	 * @throws NumberFormatException
 	 */
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		int a, b;
-		Scanner input=new Scanner (System.in);
-		while (input.hasNext())
-		{
+		Scanner input = new Scanner(System.in);
+		while (input.hasNext()) {
 			a = input.nextInt();
-			b = input.nextInt();			
-			System.out.println(a +" " + b + " " + numCicloMax(a, b));
-		} 
-		// TODO Auto-generated method stub
+			b = input.nextInt();
+			System.out.println(a + " " + b + " " + numCicloMax(a, b));
+		}
+		input.close();
 	}
-	
-	static int numCicloMax(int a, int b) 
-	{
-		int cicloMax = -1; 
-		for (int i = Math.min(a, b); i <= Math.max(a, b); i++) 
-		{
-			int con = numIteraciones(i);			
-			if (con > cicloMax) 
-			{ 
+
+	static int numCicloMax(int a, int b) {
+		int cicloMax = -1;
+		for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+			int con = numIteraciones(i);
+			if (con > cicloMax) {
 				cicloMax = con;
 			}
 		}
-		return cicloMax; 		
-	} 
-	
-	static int numIteraciones(int n) 
-	{
-		int cont = 1;		
-		while (n > 1)
-		{
-			if (n % 2 == 1) 
-			{
+		return cicloMax;
+	}
+
+	static int numIteraciones(int n) {
+		int cont = 1;
+		while (n > 1) {
+			if (n % 2 == 1) {
 				n = (3 * n) + 1;
-			}
-			else
-			{
+			} else {
 				n = n / 2;
 			}
 			cont++;
