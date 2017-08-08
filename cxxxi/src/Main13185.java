8fa4db1a353f499ca9b094606e69231f4bf53e41
Andrés Sierra <andres.sierra@beitech.co>
2017-08-08 13:50:17 -0500

    Ejercicios sobre números perfectos, abundantes y deficientes

---
+++ cxxxi/src/Main13185.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main13185 {
+
+	//TODO poner informacion
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testCases = Integer.parseInt(br.readLine());
+		int n = -1, cont;
+		StringBuffer buf = new StringBuffer();
+		while (testCases-- > 0) {
+			cont = 1;
+			n = Integer.parseInt(br.readLine());
+			
+			Set<Integer> divisors = new HashSet<Integer>();
+			for (int i = 2; i <= Math.sqrt(n); i++) {
+				if (n % i == 0) {
+					divisors.add(i);
+					divisors.add(n/i);
+				}
+			}
+			
+			for (Integer divisor : divisors) {
+					cont += divisor;
+			}
+
+
+			if (n == 1) {
+				buf.append("deficient\n");
+			} else if (cont == n) {
+				buf.append("perfect\n");
+			} else if (cont > n) {
+				buf.append("abundant\n");
+			} else {
+				buf.append("deficient\n");
+			}
+		}
+		System.out.print(buf);
+	}
+
+}
