0ba00ffbb18f43ec91760c0a7224a728288964b4
Fabio Sierra <fabres21s@gmail.com>
2017-11-29 13:02:00 -0500

    Ejercicio 10198

--- cxix/src/Main11988.java
+++ cxix/src/Main11988.java
@@ -1,25 +1,78 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 /**
- * 11988 - Broken KeyBoard (a.k.a Beiju Text)
- * @author fabio
- * No lo entiendo bien, no era esto lo que hab�a que hacer
- *
- */
-//TODO No enviado - Broken KeyBoard (a.k.a Beiju Text)
+*	11988 - Broken Keyboard (a.k.a. Beiju Text)
+*
+*
+*	Submision:	20418866
+*	Date:		2012-02-03 14:12:28
+*	Runtime:	0.400
+*	Ranking:	4077
+*/
 public class Main11988 {
 
-	public static void main(String[] args) {
+	/**
+	 * - Cadena de caracteres
+	 * 
+	 *  Todo el texto que esté entre [] ponerlo l principio de la cadena.
+	 *  Poniendo primero lo que esté más al "fondo" teniendo en cuenta
+	 *  que se puede dar [*[*] [*] *[*]]
+	 */
+	public static void main(String[] args) throws IOException {
 		String text = "This_is_a_[Beiju]_text";
-		Scanner input = new Scanner(System.in);
 
-		while (input.hasNext()) {
-			text = input.nextLine();
-			System.out.println(text.replaceAll("\\[.*\\]", ""));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean home;
 
+		StringBuffer current, output = new StringBuffer();
+		LinkedList<StringBuffer> list;
+
+		char[] array;
+		while ((text = br.readLine()) != null) {
+			home = false;
+			current = new StringBuffer();
+			list = new LinkedList<StringBuffer>();
+
+			array = text.toCharArray();
+
+			for (char c : array) {
+				if (c == '[') {
+					if (home) {
+						list.addFirst(current);
+					} else {
+						list.addLast(current);
+					}
+
+					current = new StringBuffer();
+					home = true;
+				} else if (c == ']') {
+
+					if (home) {
+						list.addFirst(current);
+					} else {
+						list.addLast(current);
+					}
+					home = false;
+					current = new StringBuffer();
+				} else {
+					current.append(c);
+				}
+			}
+
+			if (home) {
+				list.addFirst(current);
+			} else {
+				list.addLast(current);
+			}
+			for (StringBuffer answer : list) {
+				output.append(answer);
+			}
+			output.append("\n");
 		}
-
-		input.close();
+		System.out.print(output);
 
 	}
 
