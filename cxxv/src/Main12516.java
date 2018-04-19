39949e0d2ab215556050976fd4a21a983223a052
Fabio Sierra <fsierra@conexia.com>
2018-04-19 13:39:49 -0500

    Corrección ejercicio 12516 - es necesario generar casos de prueba

--- cxxv/src/Main12516.java
+++ cxxv/src/Main12516.java
@@ -1,77 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-
-//TODO TLE - Cinema-Cola
 public class Main12516 {
 
-	/**
-	 * time limit - replantear  o pasar a C++
+	
+	/*
+	 * TODO - WA generar casos de prueba
 	 */
-	public static void main(String[] args) {
-		String abc="ABCDEFGHIJKLMNOPQRSTUVWXYZ", dir,pos;
-		ArrayList <String> sinasignar;
-		Scanner input=new Scanner(System.in);
-		int row,column,persons,friends,r,c,i;
-		StringBuffer output=new StringBuffer();
-		int [][] cinema;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		ArrayList<String> sinasignar;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int row, column, persons, friends, r, c, i;
+		StringBuffer output = new StringBuffer();
+		int[][] cinema;
 		boolean correcto;
-		while(input.hasNext()){
-			row=input.nextInt();
-			column=input.nextInt();
-			if(row==column && row==0)
-				break;
-			cinema=new int[row][2*column+1];
-			persons=input.nextInt();
-			while(persons-->0){
-				dir=input.next();
-				pos=input.next();
-				r=abc.indexOf(dir.charAt(0));
-				c=Integer.valueOf(dir.substring(1));
-				c=2*c-1;
-				if(pos.equals("-"))
+
+		String line;
+		while (!(line = br.readLine()).equals("0 0")) {
+
+			args = line.split(" ");
+			row = Integer.parseInt(args[0]);
+			column = Integer.parseInt(args[1]);
+
+			cinema = new int[row][2 * column + 1];
+			persons = Integer.parseInt(br.readLine());
+			while (persons-- > 0) {
+
+				args = br.readLine().split(" ");
+				r = args[0].substring(0, 1).toCharArray()[0] - 65;
+				c = Integer.parseInt(args[0].substring(1));
+				c = 2 * c - 1;
+				if (args[1].equals("-"))
 					c--;
 				else
 					c++;
-				cinema[r][c]=1;
+				cinema[r][c] = 1;
 			}
-			friends=input.nextInt();
-			correcto=true;
-			sinasignar=new ArrayList<String>();
-			while(friends-->0){
-				dir=input.next();
-				r=abc.indexOf(dir.charAt(0));
-				c=Integer.valueOf(dir.substring(1));
-				c=2*c-1;
-				if(cinema[r][c+1]==0 && cinema[r][c-1]==1)
-					cinema[r][c+1]=1;
-				else if(cinema[r][c+1]==1 && cinema[r][c-1]==0)
-					cinema[r][c-1]=1;
-				else if(cinema[r][c+1]==1 && cinema[r][c-1]==1)
-					correcto=false;
-				else
-					sinasignar.add(dir);
+			friends = Integer.parseInt(br.readLine());
+			correcto = true;
+			sinasignar = new ArrayList<String>();
+			while (friends-- > 0) {
+				args = br.readLine().split(" ");
+				if (correcto) {
+					r = args[0].substring(0, 1).toCharArray()[0] - 65;
+					c = Integer.parseInt(args[0].substring(1));
+					c = 2 * c - 1;
+					if (cinema[r][c + 1] == 0 && cinema[r][c - 1] == 1)
+						cinema[r][c + 1] = 1;
+					else if (cinema[r][c + 1] == 1 && cinema[r][c - 1] == 0)
+						cinema[r][c - 1] = 1;
+					else if (cinema[r][c + 1] == 1 && cinema[r][c - 1] == 1)
+						correcto = false;
+					else
+						sinasignar.add(args[0]);
+				}
 			}
-			for(i=sinasignar.size()-1;i>-1;i--){
-				dir=sinasignar.get(i);
-				r=abc.indexOf(dir.charAt(0));
-				c=Integer.valueOf(dir.substring(1));
-				c=2*c-1;
-				if(cinema[r][c+1]==0 && cinema[r][c-1]==1)
-					cinema[r][c+1]=1;
-				else if(cinema[r][c+1]==1 && cinema[r][c-1]==0)
-					cinema[r][c-1]=1;
-				else if(cinema[r][c+1]==1 && cinema[r][c-1]==1)
-					correcto=false;
+
+			if (correcto) {
+				for (i = sinasignar.size() - 1; i > -1; i--) {
+					String dir = sinasignar.get(i);
+
+					r = dir.substring(0, 1).toCharArray()[0] - 65;
+					c = Integer.valueOf(dir.substring(1));
+					c = 2 * c - 1;
+					if (cinema[r][c + 1] == 0 && cinema[r][c - 1] == 1)
+						cinema[r][c + 1] = 1;
+					else if (cinema[r][c + 1] == 1 && cinema[r][c - 1] == 0)
+						cinema[r][c - 1] = 1;
+					else if (cinema[r][c + 1] == 1 && cinema[r][c - 1] == 1) {
+						correcto = false;
+						break;
+					} else {
+						System.out.println("estoy suponiendo (mal) que no llega hasta aqui");
+					}
+				}
 			}
-			if(correcto)
+			if (correcto)
 				output.append("YES\n");
 			else
 				output.append("NO\n");
 
 		}
 		System.out.print(output);
-		input.close();
 	}
 
 }
