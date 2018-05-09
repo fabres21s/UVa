384dba0e8287f84b21c33e7c27763d704f3293f4
Fabio Sierra <fsierra@conexia.com>
2018-05-08 19:30:09 -0500

    prueba 1098

--- x/src/Main1098.java
+++ x/src/Main1098.java
@@ -1,34 +1,92 @@
+import java.awt.Point;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main1098 {
- //TODO No enviado
+	// TODO No enviado
 	static int FILAS = 3;
 	static int COLUMNAS = 6;
 	static int count = 0;
+	static Map<Integer, Point> priority = new HashMap<Integer, Point>();
 
 	public static void main(String[] args) {
 		int[][] array = new int[FILAS][COLUMNAS];
 		array[0][0] = 1;
 		// solve(0, 0, array, 1);
+		priority.put(4, new Point(2, 1));
+		priority.put(9, new Point(2, 4));
 
 		array[1][0] = 2;
-		solve(1, 0, array, 2);
+		boot(array, 2, 1,0);
+		
 
 		// array[1][0] = 2;
 		// array[2][0] = 3;
 		// solve(2, 0, array, 3);
 	}
 
+	private static void boot(int[][] array, int step, int i, int j) {
+		solve(i, j, array, step);
+		
+	}
+
 	private static void solve(int i, int j, int[][] array, int step) {
+
+		Point point = priority.get(step);
+		if (point != null) {
+
+			if (i == (int) point.getX() && (j == (int) point.getY())) {
+				array[i][j] = step;
+				priority.remove(step);
+				boot(array, step, i, j);
+//				if (i - 1 > -1) {
+//					
+//					if (array[i - 1][j] == 0) {
+//						solve(i - 1, j, array, step + 1);
+//					}
+//				}
+//
+//				if ((i + 1) < FILAS) {
+//
+//					if (array[i + 1][j] == 0) {
+//						
+//						solve(i + 1, j, array, step + 1);
+//					}
+//
+//				}
+//
+//				if ((j - 1) > -1) {
+//					if (array[i][j - 1] == 0) {
+//						solve(i, j - 1, array, step + 1);
+//					}
+//				}
+//				if ((j + 1) < COLUMNAS) {
+//					if (array[i][j + 1] == 0) {
+//						solve(i, j + 1, array, step + 1);
+//
+//					}
+//				}
+				
+
+			} else {
+				for (int[] array3 : array) {
+					//System.out.println(Arrays.toString(array3));
+				}
+				System.out.println("camino imposible");
+			}
+			return;
+		}
 
 		if (i - 1 > -1) {
 
 			if (array[i - 1][j] == 0) {
 				array[i][j] = step;
 				solve(i - 1, j, array, step + 1);
-				array[i][j] = 0;
-			}
 
+				array[i][j] = 0;
+
+			}
 		}
 		if ((i + 1) < FILAS) {
 
