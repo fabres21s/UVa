b1ee6b2eb4d1025dae20c1bf195a3c93f0761e81
Fabio Andrés Sierra Jaraba - Ceiba Software <fabio.sierra@ceiba.com.co>
2020-11-23 11:53:42 -0500

    Ejercicio 10226 :)

--- ci/src/Main.java
+++ ci/src/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+	//10192
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- cii/src/Main.java
+++ cii/src/Main.java
@@ -6,40 +6,42 @@ import java.util.TreeMap;
 
 public class Main {
 
-	static  int records = 0;
-	
+	static int records = 0;
+
+	//10226
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		final BufferedReader br = new BufferedReader(new InputStreamReader(
-				System.in));
-		
+		final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder output = new StringBuilder();
+
 		int testCases = Integer.parseInt(br.readLine());
-		
+		br.readLine();
 		String specie;
-		while (testCases --> 0) {
-			if (testCases > 0) {
-				System.out.println();
-			}
-			
-			br.readLine();			
-			
+		while (testCases-- > 0) {
+
 			TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>();
-			 records = 0;
+			records = 0;
 			while (!(specie = br.readLine()).equals("")) {
 				Integer times = treeMap.get(specie);
-				
+
 				if (Objects.isNull(times)) {
 					times = 1;
 				} else {
 					times++;
 				}
-				
+
 				treeMap.put(specie, times);
 				records++;
 			}
-			
-			
-			treeMap.entrySet().stream().forEach(e -> System.out.printf("%s %.04f\n", e.getKey(), Double.valueOf(e.getValue()*100)/records));
+
+			treeMap.entrySet().stream().forEach(e -> output
+					.append(String.format("%s %.04f\n", e.getKey(), Double.valueOf(e.getValue() * 100) / records)));
+
+			if (testCases > 0) {
+				output.append("\n");
+			}
 		}
+		System.out.print(output);
 	}
 
 }
