5e0a259357602224f516cb0d8dd4fca46d664073
Fabio Andrés Sierra Jaraba - Ceiba Software <fabio.sierra@ceiba.com.co>
2020-11-25 10:02:02 -0500

    Se resuleve ejericio 10361 - faltan envíos

--- ciii/src/Main.java
+++ ciii/src/Main.java
@@ -4,63 +4,51 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+	//10361
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringBuilder output = new StringBuilder();
 		int testCases = Integer.parseInt(br.readLine());
 
-		int x1, x2, y1, y2, z1, z2;
+		int x1, x2, y1, y2;
 
-		char s1[];
-		String s2;
+		char l1[];
+		String l2;
 
 		while (testCases-- > 0) {
-			s1 = br.readLine().toCharArray();
-			s2 = br.readLine();
+			l1 = br.readLine().toCharArray();
+			l2 = br.readLine();
 
-			x1 = x2 = y1 = y2 = z1 = -1;
-			z2 = s1.length - 1;
+			x1 = x2 = y1 = y2 = -1;
 
-			for (int i = 0; i < s1.length; i++) {
-				if (s1[i] == '<') {
+			for (int i = 0; i < l1.length; i++) {
+				if (l1[i] == '<') {
 					if (x1 == -1) {
 						x1 = i;
 					} else {
 						y1 = i;
 					}
-				} else if (s1[i] == '>') {
+				} else if (l1[i] == '>') {
 					if (x2 == -1) {
 						x2 = i;
 					} else {
 						y2 = i;
 					}
-				} else if (s1[i] == ' ') {
-					if (x2 > -1 && z1 == -1) {
-						z1 = i - 1;
-					} else if (y2 > -1 && z2 == s1.length - 1) {
-						z2 = i - 1;
-					}
 				}
 			}
 
-			String seq1 = (String.copyValueOf(s1).subSequence(x1, x2 + 1)).toString();
-			String seq2 = (String.copyValueOf(s1).subSequence(y1, y2 + 1)).toString();
+			String s2 = (String.copyValueOf(l1).subSequence(x1, x2 + 1)).toString();
+			String s3 = (String.copyValueOf(l1).subSequence(x2 + 1, y1)).toString();
+			String s4 = (String.copyValueOf(l1).subSequence(y1, y2 + 1)).toString();
+			String s5 = (String.copyValueOf(l1).subSequence(y2 + 1, l1.length)).toString();
 
-			String word1 = (String.copyValueOf(s1).subSequence(x1, z1 + 1)).toString();
-			String word2 = (String.copyValueOf(s1).subSequence(y1, z2 + 1)).toString();
+			output.append(String.copyValueOf(l1).replaceAll("[<|>]", "") + "\n");
 
-			String toReplace = (String.copyValueOf(s1).subSequence(x1, z2 + 1)).toString();
-
-			String t1 = (String.copyValueOf(s1).subSequence(x1, z1 + 1).toString().replace(seq1, seq2));
-			String t2 = (String.copyValueOf(s1).subSequence(y1, z2 + 1).toString().replace(seq2, seq1));
-
-			output.append(String.copyValueOf(s1).replaceAll("[<|>]", "") + "\n");
-			output.append(
-					s2.replace("...", toReplace.replace(word2, t2.replaceAll("[<|>]", "")).replace(word1, t1.replaceAll("[<|>]", ""))) + "\n");
+			output.append(l2.replace("...", s4.concat(s3).concat(s2).concat(s5).replaceAll("[<|>]", "") + "\n"));
 
 		}
-		
+
 		System.out.print(output);
 
 	}
